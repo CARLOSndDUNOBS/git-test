78d50678bac1851743b4e374118a7e0291cb2445
Carlos Eduardo de Oliveira <dunobs@gmail.com>
2025-08-20 00:29:10 -0300

    programa feito

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-console.log('hello, world!');
-alert("olá man, você só está iniciando a brincadeira");
-alert("AS COISAS IRÃO ESQUENTAR DAQUI PRA FRENTE KKK");
+// console.log('hello, world!');
+// alert("olá man, você só está iniciando a brincadeira");
+// alert("AS COISAS IRÃO ESQUENTAR DAQUI PRA FRENTE KKK");
